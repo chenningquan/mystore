29b3da5b413d7552eeb1492a13835a60572b448a
chennq <ningquanchen@today36524.com.cn>
2020-09-24 18:01:44 +0800

    no message

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div class="container">
+    <p class="noAccount">没有账号?</p>
+    <img class="icon" src="" />
+    <p class="welcome">欢迎来到今天小店</p>
+    <p class="tip">每一盏灯都有温暖城市的梦想</p>
+    <input class="phone" type="tel" />
+    <input class="verify" type="button" />
+  </div>
 </template>
 
 <script>
@@ -8,4 +15,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="stylus" scoped>
+.container
+  background red
+  .noAccount
+    font-size .14rem
+    font-family PingFangSC-Semibold, PingFang SC
+    line-height .2rem
+</style>
